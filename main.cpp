de56d0cfbeaf10a62bca4efb5f6424d42906c595
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-11-13 12:58:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
     int n = 0, a = 0, b = 1, c = a + b;
     cin >> n;
         cout << a << endl;
-        cout << b << endl;
+       
         for (int i = 3; i <= n; i++) {
             cout << c << endl;
             a = b;
