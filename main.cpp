e21cd4b0be7e9df84168c95838e086b783eb301a
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-11-13 13:02:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 int main() {
     int n = 0, a = 0, b = 1, c = a + b;
-    cin >> n;
-        cout << a << endl;
-       
-        for (int i = 3; i <= n; i++) {
+    cin >> n ;
+    cout << a << endl;
+    cout << b << endl;
+    for (int i = 3; i<=n; i++) {
+
             cout << c << endl;
             a = b;
             b = c;
             c = a + b;
         }
-        return 0;
+
+       return 0;
     }
+
+
 
 
 
