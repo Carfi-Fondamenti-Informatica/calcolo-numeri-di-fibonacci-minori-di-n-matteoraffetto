2ed4a471a5f90e037e9ec5b0c043e392720cab18
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-11-13 18:15:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n = 0, a = 0, b = 1, c ;
-    cin >> n ;
+    int n = 0, a = 0, b = 1, c = 0;
+    cin >> n;
     cout << "1" << endl;
-    for (int i =0; i<=(n-2); i++) {
-            c = a+b;
+    if (c < n) {
+        for (int i = 0; i <= (n - 2); i++) {
+            c = a + b;
             a = b;
             b = c;
             cout << c << endl;
         }
 
-       return 0;
+        return 0;
     }
+}
 
 
 
