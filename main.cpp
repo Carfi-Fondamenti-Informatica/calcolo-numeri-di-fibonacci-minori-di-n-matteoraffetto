63fafa21f317a6961e82addd7dd589b38fbb796a
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-11-14 11:37:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n = 0, a = 1, b = 1, c = 0 ;
+    int n = 0, a = 0, b = 1, c = 0 ;
     cin >> n;
     cout << "1" << endl;
 
         for (int i = 0; i <= (n - 2); i++) {
-            if(c<n) {
+            if(c<=n) {
                 c = a + b;
                 a = b;
                 b = c;
